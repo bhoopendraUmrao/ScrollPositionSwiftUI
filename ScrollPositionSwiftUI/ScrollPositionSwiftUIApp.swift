1f889ce75ec35136d0c6342411cbb0ce854fe44a
Bhoopendra Umrao <umrao16091994@gmail.com>
2024-06-09 12:24:39 +0400

    Initial Commit

---
+++ ScrollPositionSwiftUI/ScrollPositionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollPositionSwiftUIApp.swift
+//  ScrollPositionSwiftUI
+//
+//  Created by Bhoopendra Umrao on 09/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrollPositionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
